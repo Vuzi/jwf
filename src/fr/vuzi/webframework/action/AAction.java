3afc28783ee145f78be9ed18396f667d28fa9102
Vuzi <vuzi@free.fr>
2015-04-05 17:09:33 +0200

    Repo refactoring + Adding of utils functions to abstract action class

--- src/fr/vuzi/webframework/action/AAction.java
+++ src/fr/vuzi/webframework/action/AAction.java
@@ -14,6 +14,33 @@ public abstract class AAction implements IAction {
 	private IContext context;
 
 	@Override
+	public boolean needsCredentials() {
+		return getCredentials().length > 0;
+	}
+
+	@Override
+	public boolean hasCredentials(String[] roles) {
+		String[] credentials = getCredentials();
+		
+		// Every element in roles must be in credentials
+		for(String credential : credentials) {
+			boolean found = false;
+			
+			for(String role : roles) {
+				if(role.equals(credential)) {
+					found = true;
+					break;
+				}
+			}
+			
+			if(!found) // Not found : not the right
+				return false;
+		}
+		
+		return true;
+	}
+	
+	@Override
 	public Void call() throws Exception {
 		proceed();  // Only call proceed here
 		return null;
